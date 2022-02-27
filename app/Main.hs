31c7ae2f7e9c552bf52eb0452a747074a695c2a9
Telpenarmo <telpenarmo@protonmail.com>
2022-02-27 16:20:06 +0100

    use repline

--- app/Main.hs
+++ app/Main.hs
@@ -9,20 +9,54 @@ import Typechecker
 import Types
 
 import Control.Monad.Except
+import Data.List (isPrefixOf)
+import Data.Text (Text, pack)
 import Parser (parseExpr)
 import Pretty
 import System.Console.ANSI
+import System.Console.Repline
 import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
 import System.IO (isEOF)
 
+type Repl a = HaskelineT IO a
+
+cmd :: String -> Repl ()
+cmd = proove . pack
+
+help :: String -> Repl ()
+help args = liftIO $ print $ "Help: " ++ show args
+
+opts :: Options (HaskelineT IO)
+opts =
+  [ ("help", help)
+  , ("test", test . pack)
+  ]
+
+completer :: Monad m => WordCompleter m
+completer n = return []
+
+ini :: Repl ()
+ini = liftIO $ putStrLn "Welcome to Capon!"
+
+final :: Repl ExitDecision
+final = do
+  liftIO $ putStrLn "Goodbye!"
+  return Exit
+
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    ["proove"] -> proove
-    ["test"] -> test
-    _ -> putStr "Invalid argument. Valid ones are \"proove\" and \"test\". " >> exitFailure
+main =
+  evalReplOpts $
+    ReplOpts
+      { banner = const (pure ">>> ")
+      , command = cmd
+      , options = opts
+      , prefix = Just ':'
+      , multilineCommand = Nothing
+      , tabComplete = Word0 completer
+      , initialiser = ini
+      , finaliser = final
+      }
 
 printP :: Pretty a => a -> IO ()
 printP = putStrLn . renderP
@@ -39,28 +73,24 @@ eval (Apply n) = Left <$> applyAssm n
 eval (Rewrite n) = Left <$> error "not implemented"
 eval Qed = Right <$> qed
 
-test :: IO ()
-test = do
-  t <- T.getLine
-  case parseExpr "test" t of
-    Left e -> printError e
+test :: Text -> Repl ()
+test s = do
+  case parseExpr "test" s of
+    Left e -> liftIO $ printError e
     Right e -> do
-      printP e
+      liftIO $ printP e
       case typecheck e of
-        Left e -> printError e
-        Right (t, tp) -> printP tp
-  test
+        Left e -> liftIO $ printError e
+        Right (t, tp) -> liftIO $ printP tp
 
-proove :: IO ()
-proove = do
-  putStr ">> "
-  t <- T.getLine
+proove :: Text -> Repl ()
+proove t = do
   case parseProof "proof" t of
-    Left err -> printError err
+    Left err -> liftIO $ printError err
     Right (InitProof e) -> case typecheck e of
-      Left txt -> putStrLn $ renderP txt
+      Left txt -> liftIO $ putStrLn $ renderP txt
       Right (t, tp) -> do
-        loop (proof emptyEnv t)
+        liftIO $ loop (proof emptyEnv t)
 
 loop :: Proof -> IO ()
 loop pf = do
