76e241ca9b019de26b5efd2bdfde34bfd52a4d70
Telpenarmo <telpenarmo@protonmail.com>
2022-03-02 12:00:12 +0100

    add app state and refactor a bit

--- app/Main.hs
+++ app/Main.hs
@@ -1,60 +1,82 @@
-module Main where
+{-# LANGUAGE FlexibleContexts #-}
+
+module Main (main) where
 
 import qualified Ast
 import Control.Monad
-import qualified Data.Text.IO as T
-import Proof
-import TacticParser
-import Typechecker
-import Types
-
 import Control.Monad.Except
+import Control.Monad.State
 import Data.List (isPrefixOf)
 import Data.Text (Text, pack)
 import Parser (parseExpr)
 import Pretty
+import Proof
 import System.Console.ANSI
 import System.Console.Repline
 import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
 import System.IO (isEOF)
+import TacticParser
+import Typechecker
+import Types
 
-type Repl a = HaskelineT IO a
+type IState = Maybe Proof
+type Repl a = HaskelineT (StateT IState IO) a
+
+(|>>) :: Pretty e => Either e a -> (a -> Repl ()) -> Repl ()
+ex |>> cont = case ex of
+  Left e -> liftIO $ printError e
+  Right a -> cont a
+
+infixr 1 >|>
+(>|>) :: Pretty e => (t -> Either e a) -> (a -> Repl ()) -> t -> Repl ()
+f >|> g = \x -> f x |>> g
+
+prompt :: MultiLine -> Repl String
+prompt = \case
+  MultiLine -> pure ":"
+  SingleLine ->
+    get >>= \case
+      Just pf -> pure "prooving > "
+      Nothing -> pure ">>> "
 
 cmd :: String -> Repl ()
-cmd = proove . pack
+cmd s = do
+  st <- get
+  maybe handleNewProof handleTactic st $ pack s
+ where
+  handleTactic pf = parseTactic "test" >|> flip evalTactic pf
+  handleNewProof = parseProof "theorem" >|> check >|> (updateProof . proof emptyEnv . fst)
+  check (InitProof e) = typecheck e
 
 help :: String -> Repl ()
-help args = liftIO $ print $ "Help: " ++ show args
+help args = liftIO $ putStrLn $ "Help: " ++ args
 
-opts :: Options (HaskelineT IO)
+opts :: [(String, String -> Repl ())]
 opts =
   [ ("help", help)
   , ("test", test . pack)
   ]
 
-completer :: Monad m => WordCompleter m
-completer n = return []
-
-ini :: Repl ()
-ini = liftIO $ putStrLn "Welcome to Capon!"
+completer :: (Monad m, MonadState IState m) => WordCompleter m
+completer n = return ["foo"]
 
 final :: Repl ExitDecision
-final = do
-  liftIO $ putStrLn "Goodbye!"
-  return Exit
+final = Exit <$ liftIO (putStrLn "Goodbye!")
 
 main :: IO ()
 main =
-  evalReplOpts $
+  flip evalStateT Nothing $ evalReplOpts ropts
+ where
+  ropts =
     ReplOpts
-      { banner = const (pure ">>> ")
+      { banner = prompt
       , command = cmd
       , options = opts
       , prefix = Just ':'
       , multilineCommand = Nothing
       , tabComplete = Word0 completer
-      , initialiser = ini
+      , initialiser = liftIO $ putStrLn "Welcome to Capon!"
       , finaliser = final
       }
 
@@ -67,48 +89,30 @@ printError a = do
   setSGR [SetColor Foreground Vivid White]
   putStrLn ""
 
-eval :: Tactic -> Proof -> Either (Proove Proof) (Proove Term)
-eval (Intro n) = Left <$> intro n
-eval (Apply n) = Left <$> applyAssm n
-eval (Rewrite n) = Left <$> error "not implemented"
-eval Qed = Right <$> qed
+updateProof :: Proof -> Repl ()
+updateProof pf = put (Just pf) >> liftIO (displayProof pf)
+
+evalTactic :: Tactic -> Proof -> Repl ()
+evalTactic = eval
+ where
+  eval (Intro n) = updatePF . intro n
+  eval (Apply n) = updatePF . applyAssm n
+  eval (Rewrite n) = error "not implemented"
+  eval Qed = (`run` finish) . qed
+  run :: Proove a -> (a -> Repl ()) -> Repl ()
+  run pv f = runExcept pv |>> f
+  updatePF = flip run updateProof
+  finish :: Pretty a => a -> Repl ()
+  finish t = put Nothing >> liftIO (putStr "Proof: " >> printP t)
 
 test :: Text -> Repl ()
-test s = do
-  case parseExpr "test" s of
-    Left e -> liftIO $ printError e
-    Right e -> do
-      liftIO $ printP e
-      case typecheck e of
-        Left e -> liftIO $ printError e
-        Right (t, tp) -> liftIO $ printP tp
+test = parseExpr "test" >|> typecheck >|> (liftIO . printP . fst)
 
-proove :: Text -> Repl ()
-proove t = do
-  case parseProof "proof" t of
-    Left err -> liftIO $ printError err
-    Right (InitProof e) -> case typecheck e of
-      Left txt -> liftIO $ putStrLn $ renderP txt
-      Right (t, tp) -> do
-        liftIO $ loop (proof emptyEnv t)
-
-loop :: Proof -> IO ()
-loop pf = do
-  times <- height
-  putStrLn $ replicate times '\n'
-  printP pf
-
-  t <- T.getLine
-  case parseTactic "test" t of
-    Left err -> printError err
-    Right tac ->
-      case eval tac pf of
-        Left pm -> case runExcept pm of
-          Left err -> printError err
-          Right pr -> loop pr
-        Right tm -> case runExcept tm of
-          Left err -> printError err
-          Right t -> print "Proof: " >> printP t
+displayProof :: Proof -> IO ()
+displayProof pf = do
+  times <- liftIO height
+  liftIO $ putStrLn $ replicate times '\n'
+  liftIO $ printP pf
  where
   height = do
     i <- getTerminalSize
